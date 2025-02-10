76eb33511dff45531cee88869f43b4d5cdc06461
Tabby-N <85933175+tabby123-dev@users.noreply.github.com>
2025-02-10 13:00:55 +0300

    health_analysis.js

---
+++ health_analysis.js
@@ -0,0 +1,4 @@
+    const addPatientButton = document.getElementById("addPatient");
+    const report = document.getElementById("report");
+    const btnSearch = document.getElementById('btnSearch');
+    const patients = [];
